50fe8ba2190524cf89509d742cb6ae135d8dc58a
alumno <alumno@Instituto.local>
2023-11-24 09:23:54 -0300

    24 del 11

--- script/clasi1.js
+++ script/clasi1.js
@@ -1,2 +1,16 @@
-let video = document.querySelector('#video')
-inframe.link = 'https://youtu.be/tCXGJQYZ9JA?si=aulKZnMi8XTjluxN '
+
+let imagen = document.querySelector('#remeras')
+
+
+let boton = document.querySelector('#botoncambiar')
+
+let eslaoriginal = true
+boton.onclick = function () {
+    if (eslaoriginal == true){
+        imagen.src = 'https://store.taylorswift.com/cdn/shop/files/PixinoV8iPf3i57vR782_400x.png?v=1687282789'
+   eslaoriginal == false
+    }else{
+imagen.src = 'https://store.taylorswift.com/cdn/shop/files/dKa9PRgf5GD9cYJAAjF6_400x.png?v=1687282789'
+   eslaoriginal == true
+}
+}
