18b9ede8c086f94a17ce40b03f8db026207d63f7
alumno <alumno@Instituto.local>
2023-11-27 10:45:33 -0300

    CLASE DEL 27

--- script/clasi1.js
+++ script/clasi1.js
@@ -1,16 +1,16 @@
 
-let imagen = document.querySelector('#remeras')
 
 
-let boton = document.querySelector('#botoncambiar')
-
-let eslaoriginal = true
-boton.onclick = function () {
-    if (eslaoriginal == true){
-        imagen.src = 'https://store.taylorswift.com/cdn/shop/files/PixinoV8iPf3i57vR782_400x.png?v=1687282789'
-   eslaoriginal == false
-    }else{
-imagen.src = 'https://store.taylorswift.com/cdn/shop/files/dKa9PRgf5GD9cYJAAjF6_400x.png?v=1687282789'
-   eslaoriginal == true
-}
-}
+let cambiar = false 
+let remera = document.querySelector('#remeratheeras')
+function cambiarImagen() {
+    if(cambiar == false) {
+     remera.src = 'https://store.taylorswift.com/cdn/shop/files/dKa9PRgf5GD9cYJAAjF6_400x.png?v=1687282789'
+    
+     cambiar = true
+   }else
+   {
+     remera.src = 'https://store.taylorswift.com/cdn/shop/files/PixinoV8iPf3i57vR782_400x.png?v=1687282789'
+   cambiar  = false
+   }
+   }
